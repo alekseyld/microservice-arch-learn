9008370be0fe43267eae34e741285c70a012f9a3
alekseyld <al.lisow1@gmail.com>
2022-11-07 20:18:06 +0300

    init commit
    - add customer module
    - add order module

---
+++ microservice-order/src/test/java/com/alekseyld/microserviceorder/MicroserviceOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alekseyld.microserviceorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
